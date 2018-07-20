b546ad4c7d493bb81192dce8fca051b95309267c
ahrnee <ahrndee@gmail.com>
2018-07-20 14:07:04 -0500

    Cut out anything non-cat.  Still problems

--- api/src/database/database.providers.ts
+++ api/src/database/database.providers.ts
@@ -1,11 +1,9 @@
 import * as mongoose from 'mongoose';
-import { ConfigService } from '../common/services/config.service';
-
-const configService = new ConfigService();
 
 export const databaseProviders = [
   {
-    provide: 'MongooseConnectionToken',
-    useFactory: async (): Promise<typeof mongoose> => await mongoose.connect(configService.mongoConnectionUri),
+    provide: 'DbConnectionToken',
+    useFactory: async (): Promise<typeof mongoose> =>
+      await mongoose.connect('mongodb://localhost/nest'),
   },
 ];
--- api/src/webAppHealthCheck.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class WebAppHealthCheckService {
-  public root(): string {
-    return 'UP';
-  }
-}

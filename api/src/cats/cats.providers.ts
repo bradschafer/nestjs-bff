9915c0cb68a29fd8b0f6117b8cd7cf6586ad9f82
ahrnee <ahrndee@gmail.com>
2018-07-20 14:10:52 -0500

    Identical Cat Code

--- api/src/cats/cats.providers.ts
+++ api/src/cats/cats.providers.ts
@@ -1,9 +1,10 @@
+import { Connection } from 'mongoose';
 import { CatSchema } from './schemas/cat.schema';
 
 export const catsProviders = [
   {
     provide: 'CatModelToken',
-    useFactory: mongoose => mongoose.connection.model('Cat', CatSchema),
-    inject: ['MongooseConnectionToken'],
+    useFactory: (connection: Connection) => connection.model('Cat', CatSchema),
+    inject: ['DbConnectionToken'],
   },
 ];
--- api/src/cats/dto/create-cat.dto.ts
+++ api/src/cats/dto/create-cat.dto.ts
@@ -1,9 +1,5 @@
-import { IsString, IsInt } from 'class-validator';
-
 export class CreateCatDto {
-  @IsString() public readonly name: string;
-
-  @IsInt() public readonly age: number;
-
-  @IsString() public readonly breed: string;
+  readonly name: string;
+  readonly age: number;
+  readonly breed: string;
 }
